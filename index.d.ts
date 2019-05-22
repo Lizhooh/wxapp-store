219679c11793d6ba7948d6916cce92da65a47abf
lizhooh <960316408@qq.com>
2019-05-22 22:10:47 +0800

    update: index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -13,11 +13,12 @@ export interface Error {
 }
 
 export default class Store {
-    get(key: string): any | Promise<any, Error>
-    set(key: string, value: any): any | Promise<any, Error>
-    remove(key: string): any | Promise<any, Error>
-    clear(): any | Promise<any, Error>
-    info(): Info | Promise<Info, Error>
-    keys(exp: string | RegExp): string[] | Promise<string[], Error>
+    constructor(opt: { wx: any })
+    get(key: string): any | Promise<any>
+    set(key: string, value: any): any | Promise<any>
+    remove(key: string): any | Promise<any>
+    clear(): any | Promise<any>
+    info(): Info | Promise<Info>
+    keys(exp: string | RegExp): string[] | Promise<string[]>
 }
 
